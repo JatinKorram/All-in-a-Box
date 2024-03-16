ae989b2252f36eb18518e81c7b06c2330e86cfde
Jatin <127529571+JatinKorram@users.noreply.github.com>
2024-03-16 17:00:32 +0530

    Add files via upload

--- level/level.gd
+++ level/level.gd
@@ -1,32 +1,29 @@
 extends Node2D
+class_name Level
 
 enum Tool {
 	NONE = 0,
-	ARMOR = 1,
-	HOVER = 2,
-	KEY = 3,
+	KEY = 1,
+	ARMOR = 2,
+	HOVER = 3,
 }
 
 @export var tilemap: TileMap
-@export var player_pos: Vector2i = Vector2i.ZERO
-@export var player_active_tool: Tool = Tool.NONE
+# 0 = Player, Rest = Inventory 
+@export var all_tools: Array[Tool] = [Tool.NONE]
+@export var cell_map: CellMap
 
-@export var inventory_tools: Array = []
-
-@export var CELL_EMPTY: Vector2i = Vector2i(-1, -1)
-@export var CELL_PLAYER: Vector2i = Vector2i(1, 0)
-@export var CELL_INVENTORY: Vector2i = Vector2i(2, 0)
-@export var CELL_IMMOVABLE: Vector2i = Vector2i(0, 1)
-@export var CELL_FINISH: Vector2i = Vector2i(1, 1)
-@export var CELL_SPIKES: Vector2i = Vector2i(2, 1)
-@export var CELL_HOLE: Vector2i = Vector2i(2, 2)
-@export var CELL_DOOR: Vector2i = Vector2i(1, 2)
-
+var player_pos: Vector2i = Vector2i.ZERO
 var cell_size: float = 0.0
 var paused: bool = false
 
+signal init_ui(all_tools: Array[Tool], cell_size: float)
+signal toggle_inventory_ui(player_pos: Vector2, inventory_pos: Vector2)
+
 func _ready():
+	player_pos = tilemap.get_used_cells_by_id(0, -1, cell_map.PLAYER)[0]
 	cell_size = tilemap.rendering_quadrant_size * tilemap.scale.x
+	init_ui.emit(all_tools, cell_size)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("Move.Up"):
@@ -56,18 +53,24 @@ func succeed():
 	# Show win screen
 
 func interact():
-	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.UP) == CELL_INVENTORY or tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.DOWN) == CELL_INVENTORY or tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.LEFT) == CELL_INVENTORY or tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.RIGHT) == CELL_INVENTORY:
-		print_debug("Open Inventory!")
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.UP) == cell_map.INVENTORY:
+		toggle_inventory_ui.emit(player_pos * cell_size, (player_pos + Vector2i.UP) * cell_size)
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.DOWN) == cell_map.INVENTORY:
+		toggle_inventory_ui.emit(player_pos * cell_size, (player_pos + Vector2i.DOWN) * cell_size)
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.LEFT) == cell_map.INVENTORY:
+		toggle_inventory_ui.emit(player_pos * cell_size, (player_pos + Vector2i.LEFT) * cell_size)
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.RIGHT) == cell_map.INVENTORY:
+		toggle_inventory_ui.emit(player_pos * cell_size, (player_pos + Vector2i.RIGHT) * cell_size)
 	
-	if player_active_tool != Tool.KEY:
+	if all_tools[0] != Tool.KEY:
 		return
-	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.UP) == CELL_DOOR:
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.UP) == cell_map.DOOR:
 		tilemap.set_cell(0, player_pos + Vector2i.UP)
-	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.DOWN) == CELL_DOOR:
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.DOWN) == cell_map.DOOR:
 		tilemap.set_cell(0, player_pos + Vector2i.DOWN)
-	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.LEFT) == CELL_DOOR:
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.LEFT) == cell_map.DOOR:
 		tilemap.set_cell(0, player_pos + Vector2i.LEFT)
-	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.RIGHT) == CELL_DOOR:
+	if tilemap.get_cell_atlas_coords(0, player_pos + Vector2i.RIGHT) == cell_map.DOOR:
 		tilemap.set_cell(0, player_pos + Vector2i.RIGHT)
 
 func move_player(direction: Vector2i):
@@ -78,11 +81,11 @@ func move_player(direction: Vector2i):
 func try_push_to_cell(cell: Vector2i, direction: Vector2i) -> bool:
 	var previous_cell_type: Vector2i = tilemap.get_cell_atlas_coords(0, cell - direction)
 	var current_cell_type: Vector2i = tilemap.get_cell_atlas_coords(0, cell)
-	if current_cell_type == CELL_IMMOVABLE or current_cell_type == CELL_DOOR:
+	if current_cell_type == cell_map.WALL or current_cell_type == cell_map.DOOR:
 		return false
-	if current_cell_type == CELL_EMPTY or try_push_to_cell(cell + direction, direction):
+	if current_cell_type == cell_map.EMPTY or try_push_to_cell(cell + direction, direction):
 		tilemap.set_cell(0, cell, 0, previous_cell_type)
-		if previous_cell_type == CELL_PLAYER:
+		if previous_cell_type == cell_map.PLAYER:
 			tilemap.set_cell(0, cell - direction)
 		process_cell_triggers(cell, previous_cell_type)
 		return true
@@ -90,21 +93,26 @@ func try_push_to_cell(cell: Vector2i, direction: Vector2i) -> bool:
 
 func process_cell_triggers(cell: Vector2i, triggerer_type: Vector2i) -> void:
 	var cell_type: Vector2i = tilemap.get_cell_atlas_coords(1, cell)
-	if cell_type == CELL_EMPTY:
+	if cell_type == cell_map.EMPTY:
 		return
-	if cell_type == CELL_HOLE:
-		if triggerer_type == CELL_PLAYER:
+	if cell_type == cell_map.HOLE:
+		if triggerer_type == cell_map.PLAYER:
 			# Remember to actually pause the gameplay when
 			# the player fails! Not doing so will surely
 			# result in bugs.
-			if player_active_tool == Tool.HOVER:
+			if all_tools[0] == Tool.HOVER:
 				return
 			fail()
 		tilemap.set_cell(0, cell)
-	if triggerer_type == CELL_PLAYER:
-		if cell_type == CELL_SPIKES and not player_active_tool == Tool.ARMOR:
+	if triggerer_type == cell_map.PLAYER:
+		if cell_type == cell_map.SPIKES and not all_tools[0] == Tool.ARMOR:
 			print_debug("Fail!")
 			fail()
-		if cell_type == CELL_FINISH:
+		if cell_type == cell_map.FINISH:
 			print_debug("Finish!")
 			succeed()
+
+func _on_ui_handler_drag_and_drop_done(from_slot, to_slot):
+	var tool: Tool = all_tools[to_slot]
+	all_tools[to_slot] = all_tools[from_slot]
+	all_tools[from_slot] = tool
---
+++ ui/ui_handler.gd
@@ -0,0 +1,70 @@
+extends Control
+class_name UIHandler
+
+@export var slots: Control = null
+
+var slot_sprites: Array[Sprite2D] = []
+var slot_sprite_size: float = 0
+var mouse_held_sprite: Sprite2D = null
+var mouse_held_sprite_index: int = -1
+var mouse_held_sprite_original_global_pos: Vector2 = Vector2.ZERO
+
+@onready var slot_resource: Resource = preload("res://ui/slot.tscn")
+
+signal drag_and_drop_done(from_slot: int, to_slot: int)
+
+func _on_tests_init_ui(all_tools: Array[Level.Tool], cell_size: float):
+	slot_sprite_size = cell_size / 3
+	slot_sprites.resize(all_tools.size() + 1)
+	var x_offset: float = 0.0
+	for i in range(0, all_tools.size()):
+		var slot_sprite: Sprite2D = slot_resource.instantiate()
+		slot_sprite.position.x = x_offset
+		x_offset += cell_size
+		slot_sprite.frame_coords = tool_to_atlas_coords(all_tools[i])
+		slot_sprites[i] = slot_sprite
+		slots.add_child(slot_sprite)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Mouse.Left"):
+		for i in range(0, slot_sprites.size() - 1):
+			var slot_sprite: Sprite2D = slot_sprites[i]
+			var offset: Vector2 = slot_sprite.get_local_mouse_position()
+			if slot_sprite.frame_coords != Vector2i(0, 2) and offset.x >= 0 and offset.x < slot_sprite_size and offset.y >= 0 and offset.y < slot_sprite_size:
+				mouse_held_sprite_original_global_pos = slot_sprite.global_position
+				mouse_held_sprite_index = i
+				mouse_held_sprite = slot_sprite
+				break
+	
+	if Input.is_action_just_released("Mouse.Left"):
+		if mouse_held_sprite_index == -1:
+			return
+		for i in range(0, slot_sprites.size() - 1):
+			if i == mouse_held_sprite_index:
+				continue
+			var slot_sprite: Sprite2D = slot_sprites[i]
+			var offset: Vector2 = slot_sprite.get_local_mouse_position()
+			mouse_held_sprite.global_position = mouse_held_sprite_original_global_pos
+			if slot_sprite.frame_coords == Vector2i(0, 2) and offset.x >= 0 and offset.x < slot_sprite_size and offset.y >= 0 and offset.y < slot_sprite_size:
+				var mouse_held_sprite_frame_coords: Vector2i = slot_sprite.frame_coords
+				slot_sprite.frame_coords = mouse_held_sprite.frame_coords
+				mouse_held_sprite.frame_coords = mouse_held_sprite_frame_coords
+				drag_and_drop_done.emit(mouse_held_sprite_index, i)
+				break
+		mouse_held_sprite_index = -1
+		mouse_held_sprite = null
+
+func _input(event):
+	if mouse_held_sprite_index == -1:
+		return
+	if event is InputEventMouseMotion:
+		mouse_held_sprite.global_position = get_canvas_transform().affine_inverse() * event.global_position
+
+func tool_to_atlas_coords(tool: Level.Tool) -> Vector2i:
+	if tool == Level.Tool.KEY:
+		return Vector2i(3, 0)
+	if tool == Level.Tool.ARMOR:
+		return Vector2i(3, 1)
+	if tool == Level.Tool.HOVER:
+		return Vector2i(3, 2)
+	return Vector2i(0, 2)
