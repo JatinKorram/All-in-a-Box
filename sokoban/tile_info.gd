d811d80f0d30e31e2edfea87caddb708efe37af2
JatinKorram <jatinkorram0@gmail.com>
2024-03-23 01:20:35 +0530

    Added collision layer and mask to TileInfo

--- sokoban/tile_info.gd
+++ sokoban/tile_info.gd
@@ -1,7 +1,14 @@
 extends Resource
 class_name TileInfo
 
-@export var atlas_coords: Vector2i = Vector2i(-1, -1)
-@export var collision: bool = true
-@export var z_index: int = 0
+@export var name: String
 @export var attached_script: Script = null
+@export var atlas_coords: Vector2i = Vector2i(-1, -1)
+
+@export_group("Visual")
+@export var z_index: int = 0
+
+@export_group("Physics")
+@export var is_trigger: bool = false
+@export_flags_2d_physics var collision_layer: int = 1
+@export_flags_2d_physics var collision_mask: int = 1
--- test_resources/tiles/flag.gd
+++ test_resources/tiles/flag.gd
@@ -2,4 +2,7 @@ extends Area2D
 class_name Flag
 
 func _ready():
-	pass
+	area_entered.connect(_on_flag_area_entered)
+
+func _on_flag_area_entered(_area: Area2D):
+	print_debug("Flag entered!")
